c40cd484b8ac724a67c976ff6409797b56470ca1
Shane Orend <wagen00@gmail.com>
2018-12-14 09:19:16 -0500

    pages for test and bookmark added

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,8 +6,20 @@ module.exports = function(app) {
     app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "../index.html"));
     });
-    app.get("/test", function(req, res) {
-        res.sendFile(path.join(__dirname, "../pages/categories.html"));
+    app.get("/5thgrade", function(req, res) {
+        res.sendFile(path.join(__dirname, "../pages/5thgrade.html"));
+    });
+    app.get("/7thgrade", function(req, res) {
+        res.sendFile(path.join(__dirname, "../pages/7thgrade.html"));
+    });
+    app.get("/8thgrade", function(req, res) {
+        res.sendFile(path.join(__dirname, "../pages/8thgrade.html"));
+    });
+	app.get("/allGradesTest", function(req, res) {
+        res.sendFile(path.join(__dirname, "../pages/allTest.html"));
+    });
+    app.get("/bookmarks", function(req, res) {
+        res.sendFile(path.join(__dirname, "../pages/bookmarks.html"));
     });
    
 
